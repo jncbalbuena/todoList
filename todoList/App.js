ac15c033cc9c2d24a15b50149bdfe6ba1c343ee8
jobellebalbuena <jobelleninacbalbuena@gmail.com>
2021-09-09 17:45:56 +0800

    Manage Add Task

--- todoList/App.js
+++ todoList/App.js
@@ -1,9 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import Task from './components/Task.js';
 
 export default function App() {
+const [task,setTask] = useState();
+const [taskItems,setTaskItems]= useState([]);
+
+
+const handleAddTask = () => {
+  setTaskItems([...taskItems,task])
+  setTask(null);
+}
   return (
     <View style={styles.container}>
            {/* Today's Tasks */}
@@ -11,8 +18,14 @@ export default function App() {
         <Text style={styles.sectionTitle}>Today's Tasks</Text>
         <View style={styles.items}>
           {/* This is where the tasks will go! */}
-          <Task text={'Task 1'} />
-          <Task text={'Task 2'} />
+          {
+            taskItems.map((item,index) =>{
+              return <Task key={index} text={item} />
+
+            })
+          }
+          {/*<Task text={'Task 1'} />
+          <Task text={'Task 2'} /> */}
         </View>
       </View>
       {/* Write a task */}
@@ -20,8 +33,8 @@ export default function App() {
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.writeTaskWrapper}
       >
-          <TextInput style={styles.input} placeholder={'Write a task'} />
-          <TouchableOpacity>
+          <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)}/>
+          <TouchableOpacity onPress ={() => handleAddTask()}>
             <View style={styles.addWrapper}>
               <Text style={styles.addText}>+</Text>
             </View>
